8e1cb9a309213e80c479232ac23c3fa17f303975
ashley nordan <abuxbaum@mcad.edu>
2018-02-04 21:23:22 -0600

    week 3 final commit
    I surrender!

--- wk3-final/code/inc/sections/fun/section-template.php
+++ wk3-final/code/inc/sections/fun/section-template.php
@@ -1,21 +1,21 @@
 <h1>Fun</h1>
 
 <section>
-  <a href="/?section=fun&article=article-4"></a>
-      <img src="./img/movie.jpg" alt="img of book cover">
+  <a href="./?section=fun&article=article-4">
+      <img src="./img/movie.jpg" alt="img of book cover"></a>
       <h2>Movies with friends is always a good idea.</h2>
 </section>
  
  <section>
-  <a href="/?section=fun&article=article-5"></a>
-      <img src="./img/game.jpg" alt="img of book cover">
+  <a href="./?section=fun&article=article-5">
+      <img src="./img/game.jpg" alt="img of book cover"></a>
       <h2>Play a game with friends</h2>
  </section>   
 
 
  
 </section>
-  <a href="/?section=fun&article=article-6"></a>
-      <img src="./img/music.jpg" alt="img of book cover">
+  <a href="./?section=fun&article=article-6">
+      <img src="./img/music.jpg" alt="img of book cover"></a>
       <h2>Listening to music is so much fun</h2>
 </section>
--- wk3-final/code/inc/sections/science/section-template.php
+++ wk3-final/code/inc/sections/science/section-template.php
@@ -1,21 +1,21 @@
 <h1>Science</h1>
 
 <section>
-  <a href="/?section=science&article=article-7"></a>
-      <img src="./img/stars.jpg" alt="img of book cover">
+  <a href="./?section=science&article=article-7">
+      <img src="./img/stars.jpg" alt="img of book cover"></a>
       <h2>Stargazing</h2>
 </section>
  
  <section>
-  <a href="/?section=science&article=article-8"></a>
-      <img src="./img/space.jpg" alt="img of book cover">
+  <a href="./?section=science&article=article-8">
+      <img src="./img/space.jpg" alt="img of book cover"></a>
       <h2>Telescopes give you pinkeye</h2>
  </section>   
 
 
  
 </section>
-  <a href="/?section=science&article=article-9"></a>
-      <img src="./img/future.jpg" alt="img of book cover">
+  <a href="./?section=science&article=article-9">
+      <img src="./img/future.jpg" alt="img of book cover"></a>
       <h2>What does the future hold?</h2>
 </section>
